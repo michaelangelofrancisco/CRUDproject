4e25a95ef3c729682205d507062daaceb705a25e
michaelangelofrancisco <57524808+michaelangelofrancisco@users.noreply.github.com>
2021-01-10 19:52:23 +0800

    Updated css design

--- main.js
+++ main.js
@@ -8,4 +8,19 @@ addToDoButton.addEventListener('click', function()
 	paragraph.classList.add('paragraph.styling')
 	paragraph.innerText = inputField.value;
 	toDoContainer.appendChild(paragraph);
+	inputField.value = "";
+	paragraph.addEventListener('click', function()
+	{
+		paragraph.style.textDecoration = "line-through";
+	})
+
+
+	paragraph.addEventListener('dblclick', function()
+	{
+		toDoContainer.removeChild(paragraph);
+	})
+
+	
 })
+
+
